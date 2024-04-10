03442318b1012ba77e3f6adcbf8a2d381e61442a
Seb070505 <sebastianmeinertz0705@gmail.com>
2024-04-10 14:40:08 +0200

    begyndelse på datter

--- Scripts/Daughter.gd
+++ Scripts/Daughter.gd
@@ -3,3 +3,11 @@ extends Node2D
 onready var animation = $AnimationPlayer
 
 
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	pass # Replace with function body.
