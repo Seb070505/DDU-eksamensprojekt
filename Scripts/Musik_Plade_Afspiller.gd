9ad4c83e448933756367ed1a27a0386c4f81c6f1
Seb070505 <sebastianmeinertz0705@gmail.com>
2024-04-15 15:12:43 +0200

    musikplade afspiller

---
+++ Scripts/Musik_Plade_Afspiller.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+func _physics_process(delta):
+	if Global.interact == true and Global.musikplade == 1:
+		$AudioStreamPlayer.play()
+		Global.musikplade -= 1
+		print("musik")
+	
+
+func _on_Musik_Plade_Afspiller_body_entered(body):
+	if body.has_method("player"):
+		Global.interactZone = true
+
+
+func _on_Musik_Plade_Afspiller_body_exited(body):
+	Global.interact = false
+	Global.interactZone = false
