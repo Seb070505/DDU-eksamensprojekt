b280792413fd8e363647feda71732c8f2802234e
Seb070505 <sebastianmeinertz0705@gmail.com>
2024-04-16 18:14:06 +0200

    lidt ændringer på player animationernes implementering

--- Scripts/Daughter.gd
+++ Scripts/Daughter.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var animation = $AnimationPlayer
+
 var snakkeMedDatter =  false
 
 func _physics_process(delta):
@@ -7,6 +9,7 @@ func _physics_process(delta):
 		$Panel.visible = true
 		$Label.visible = true
 		Global.mulighedForAtTageBussen = true
+		animation.play("Daughter idle")
 	else:
 		$Panel.visible = false
 		$Label.visible = false
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,33 +4,45 @@ onready var animation = $AnimationPlayer
 
 var speed = 100
 var score = 0
+var idle = true
 var pickUp_ip = false
 
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO * delta
 	
+	interact_and_pickUp()
+	
 	if Input.is_action_pressed("move_up") and pickUp_ip == false:
 		velocity.y -= 1
+		idle = false
 		animation.play("walk back")
 	
 	elif Input.is_action_pressed("move_down") and pickUp_ip == false:
 		velocity.y += 1
+		idle = false
 		animation.play("walk front")
 	
 	elif Input.is_action_pressed("move_left") and pickUp_ip == false:
 		velocity.x -= 1
+		idle = false
 		animation.play("walk left")
 	
 	elif Input.is_action_pressed("move_right") and pickUp_ip == false:
 		velocity.x += 1
+		idle = false
 		animation.play("walk right")
 	
+	if velocity.x == 0 and velocity.y == 0 and pickUp_ip ==  false:
+		idle = true
+		idle_animation()
 	
 	velocity = velocity.normalized()
 	move_and_slide(velocity * speed)
 	
 	
+
+func interact_and_pickUp():
 	if Input.is_action_pressed("Interact") and Global.interactZone == true:
 		Global.interact = true
 	
@@ -41,12 +53,36 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("pickUp") and Global.pickUpZone == true:
 		Global.pickUp = true
+		pickUp_ip = true
+		print(pickUp_ip)
+		if Input.is_action_just_released("move_down"):
+			animation.play("pick up front")
+		elif Input.is_action_just_released("move_up"):
+			animation.play("pick up back")
+		elif Input.is_action_just_released("move_left"):
+			animation.play("pick up left")
+		elif Input.is_action_just_released("move_right"):
+			animation.play("pick up right")
+		
+		if pickUp_ip == true:
+			$pickUp_ip.start()
 	
 	if Global.pickUpZone == true and Global.pickUp == false:
 		$Interact/Label2.visible = true
 	else:
 		$Interact/Label2.visible = false
 
+func idle_animation():
+	if idle == true:
+		if Input.is_action_just_released("move_down"):
+			animation.play("idle front")
+		elif Input.is_action_just_released("move_up"):
+			animation.play("idle back")
+		elif Input.is_action_just_released("move_left"):
+			animation.play("idle left")
+		elif Input.is_action_just_released("move_right"):
+			animation.play("idle right")
+	
 
 func player():
 	pass
@@ -54,3 +90,9 @@ func player():
 
 func collected_music(value):
 	score += value
+
+
+func _on_pickUp_ip_timeout():
+	pickUp_ip = false
+	print("pickUp_ip er")
+	print(pickUp_ip)
