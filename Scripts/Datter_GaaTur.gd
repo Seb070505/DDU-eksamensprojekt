1c5ac0b063d0dfb108b950cdd4d31458c79669ab
Seb070505 <sebastianmeinertz0705@gmail.com>
2024-04-20 20:14:46 +0200

    work in progress

--- Scripts/Datter_GaaTur.gd
+++ Scripts/Datter_GaaTur.gd
@@ -3,10 +3,28 @@ extends KinematicBody2D
 onready var animation = $AnimationPlayer
 
 var player = null
+var speed = 75
+var motion = Vector2.ZERO
+var stop_motion = false
 
 func _physics_process(delta):
+	motion = Vector2.ZERO
 	
-	pass
+	if player:
+		if stop_motion == false:
+			motion = position.direction_to(player.position) * speed
+	
+	motion = move_and_slide(motion)
+	
+	if motion.x == 0 and motion.y == 0:
+		animation.play("idle")
+	
+	if motion.x > 0:
+		animation.play("walking_right")
+	
+	if motion.x < 0:
+		animation.play("walking_left")
+
 
 func _on_Area2D_body_entered(body):
 	if body.has_method("player"):
@@ -15,3 +33,12 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D_body_exited(body):
 	player = null
+
+
+func _on_Area2D2_body_entered(body):
+	if body.has_method("player"):
+		stop_motion = true
+
+
+func _on_Area2D2_body_exited(body):
+	stop_motion = false
