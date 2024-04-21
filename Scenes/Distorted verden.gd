8c63d4c18b96a198732aa51700dbd5d8e4af7910
Seb070505 <sebastianmeinertz0705@gmail.com>
2024-04-21 17:05:35 +0200

    brandmand og sidste interaktion med datter

--- Scenes/Distorted verden.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-
-func _on_Area2D_body_entered(body):
-	if body.has_method("player"):
-		get_tree().change_scene("res://Scenes/MainGame.tscn")
-		Global.brandStation = true
---
+++ Scripts/Brandmand.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+onready var animation = $AnimationPlayer
+
+func _physics_process(delta):
+	if Global.interact == true:
+		$Panel.visible = true
+		$Label.visible = true
+		Global.snakketMedBrandmand = true
+		Global.objektiv_snakMedDatter2 = true
+		animation.play("idle")
+	else:
+		$Panel.visible = false
+		$Label.visible = false
+
+
+func _on_Area2D_body_entered(body):
+	if body.has_method("player") and Global.brandStation == true:
+		Global.interactZone = true
+
+
+func _on_Area2D_body_exited(body):
+	Global.interactZone = false
+	Global.interact = false
+	
